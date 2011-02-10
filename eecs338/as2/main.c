f1eb89dcfbb25dcb04c377d1b3b48ecebc7135e2
eskimod <ronin8@gmail.com>
2011-02-10 02:55:18 -0500

    added forks and pipe creation

--- eecs338/as2/main.c
+++ eecs338/as2/main.c
@@ -8,12 +8,62 @@
 
 #define READ 0 // for use in the pipes
 #define WRITE 1
+#define MAX_BUFFER_SIZE 256
 
 int main(int argc, char* argv[])
 {
-	int pipe_a[2];
-	int pipe_b[2];
-	int pipe_c[2];
-	int pipe_d[2];
-	
+	int ref_to_p1[2];
+	int p1_to_ref[2];
+	int ref_to_p2[2];
+	int p2_to_ref[2];
+    pid1_t pid1, pid2;
+    char buffer[MAX_BUFFER_SIZE];
+
+    //initialize pipes
+    if(pipe(ref_to_p1) == -1)
+    {
+        perror("main:ref:pipe1");
+        exit(EXIT_FAILURE);
+    }
+    if(pipe(p1_to_ref) == -1)
+    {
+        perror("main:ref:pipe2");
+        exit(EXIT_FAILURE);
+    }    
+    if(pipe(ref_to_p2) == -1)
+    {
+        perror("main:ref:pipe3");
+        exit(EXIT_FAILURE);
+    }    
+    if(pipe(p2_to_ref) == -1)
+    {
+        perror("main:ref:pipe4");
+        exit(EXIT_FAILURE);
+    }
+
+    //fork twice, error handle
+    pid1 = fork();
+    if(pid1 == -1)
+    {
+        perror("main:ref:fork1");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("child 1 with pid %5d spawned.\n",pid1);
+    }
+    
+    //second fork
+	if(pid1 != 0) {
+		//must be parent
+		pid2 = fork();
+        if(pid2 == -1)
+        {
+            perror("main:ref:fork2");
+            exit(EXIT_FAILURE);
+        } else {
+        printf("child 2 with pid %5d spawned.\n",pid2);
+    }
+
+	}
+
+return 0;
 }
