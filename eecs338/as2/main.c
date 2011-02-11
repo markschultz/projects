06e1746f972842a57ba2ceacabddc71e72cc2831
eskimod <ronin8@gmail.com>
2011-02-10 19:11:29 -0500

    added 2 more pipes for children to talk back - now final

--- eecs338/as2/main.c
+++ eecs338/as2/main.c
@@ -13,9 +13,13 @@
 #define WRITE 1
 #define COOP "1"
 #define DEF "0"
+#define ASK "1"
+#define TERM "0"
 #define str_eq(s1,s2)  (!strcmp ((s1),(s2)))
 int p1_to_ref[2];
 int p2_to_ref[2];
+int ref_to_p1[2];
+int ref_to_p2[2];
 
 int main(int argc, char* argv[])
 {
@@ -37,7 +41,16 @@ int main(int argc, char* argv[])
         perror("main:ref:pipe2");
         exit(EXIT_FAILURE);
     }
-
+    if(pipe(ref_to_p1) == -1)
+    {
+        perror("main:ref:pipe3");
+        exit(EXIT_FAILURE);
+    }
+    if(pipe(ref_to_p2) == -1)
+    {
+        perror("main:ref:pipe4");
+        exit(EXIT_FAILURE);
+    }
     //fork twice, error handle
     pid1 = fork();
     if(pid1 == -1)
@@ -63,20 +76,25 @@ int main(int argc, char* argv[])
     
     //now start the game
     int i;
-    for(i = 0; i<10; i++) {
+    for(i = 0; i<=10; i++) {
     
         
         //ref behavior
-        if(pid1 > 0 && pid2 > 0) { //parent
-            printf("Game %d:\n",i);
-        }
         char buffer1[10];
         char buffer2[10];
         //ref behavior
         if(pid1 > 0 && pid2 > 0) { //parent
-            read(p1_to_ref[READ],buffer1,2);
-            //int *intbuf;
-            //read(p1_to_ref[READ],intbuf,sizeof(int)); //my attempt at passing integers in the pipe
+            if(i==10){
+                //terminate children
+                write(ref_to_p1[WRITE],TERM,strlen(TERM)+1);
+                write(ref_to_p2[WRITE],TERM,strlen(TERM)+1);
+                break;
+            } else {
+                printf("Game %d:\n",i+1);
+                write(ref_to_p1[WRITE],ASK,strlen(ASK)+1);//ask 2 children for numbers
+                write(ref_to_p2[WRITE],ASK,strlen(ASK)+1);
+            }
+            read(p1_to_ref[READ],buffer1,2);//read for responses
             read(p2_to_ref[READ],buffer2,2);
             if(str_eq(buffer1,COOP) && str_eq(buffer2,COOP)) {
                 scores[0]+=6;
@@ -96,24 +114,37 @@ int main(int argc, char* argv[])
                 perror("do_game:ref:read");
                 exit(EXIT_FAILURE);
             }
+            
         } else if(pid1 == 0) { //child1
             //player1 behavior 
-            //write random number
-            //srand(time(NULL));
-            /*/itoa(rand()%2,choice,10);
-            sprintf(choice,"%d",rand()%2);
-            printf("%s",choice);
-            fflush(NULL);
-            write(p1_to_ref[WRITE],(int*)(rand()%2),sizeof(int));
-            *///too much work to convert random int to string with correct length
             char* choice = "1";
-            write(p1_to_ref[WRITE],choice,strlen(choice)+1);//(rand() % 2),1);
+            char buf[5];
+            read(ref_to_p1[READ],buf,2);
+            if(str_eq(buf,ASK)) {
+                write(p1_to_ref[WRITE],choice,strlen(choice)+1);//(rand() % 2),1);
+            } else if (str_eq(buf,TERM)) {
+                //printf("Player1 exiting...\n");
+                fflush(NULL);
+                _exit(EXIT_SUCCESS);
+            } else {
+                perror("main:p1:read");
+                _exit(EXIT_FAILURE);
+            }
         } else if(pid2 == 0) { //child2
             //player2 behavior
-            //write random number
-            srand(time(NULL));
             char* choice = "0";
-            write(p2_to_ref[WRITE],choice,strlen(choice)+1);//(rand() % 2),1);
+            char buf[5];
+            read(ref_to_p2[READ],buf,2);
+            if(str_eq(buf,ASK)) {
+                write(p2_to_ref[WRITE],choice,strlen(choice)+1);//(rand() % 2),1);
+            } else if (str_eq(buf,TERM)) {
+                //printf("Player2 exiting...\n");
+                fflush(NULL);
+                _exit(EXIT_SUCCESS);
+            } else {
+                perror("main:p2:read");
+                _exit(EXIT_FAILURE);
+            }        
         }
     }
     //print total and cleanup pipes
@@ -123,6 +154,10 @@ int main(int argc, char* argv[])
         printf("%d: %d months\n",pid2,scores[1]);
 
         //close all pipes
+        close(ref_to_p1[READ]);
+        close(ref_to_p1[WRITE]);
+        close(ref_to_p2[READ]);
+        close(ref_to_p2[WRITE]);
         close(p2_to_ref[READ]);
         close(p2_to_ref[WRITE]);
         close(p1_to_ref[READ]);
