adc49d01af72fcfe7c0fd8e2c7969572bdf5104b
eskimod <ronin8@gmail.com>
2011-04-15 23:45:15 -0400

    fixed makefile and git

--- eecs338/as7/cookie_server.c
+++ eecs338/as7/cookie_server.c
@@ -5,32 +5,39 @@
  */
 
 #include "cookie.h"
+int cookie = 20;
+int tina = 0;
 
 int *get_cookie_1_svc(int *argp, struct svc_req *rqstp)
 {
-	static int  result;
-	int cookie = 20;
-	int tina = 0;
+	static int result;
+
 	int client = *argp;
 	
-	if (cookie >= 0){ //are there enough cookies?
+	if (cookie > 0){ //are there enough cookies?
 		if (client==1){ //tina
+			//printf("tina: req\n");
 			tina++; //keep track of how many tina has had
 			cookie--;
-			return 1; // 0=empty,1=yes,2=no
+			result= 1; // 0=empty,1=yes,2=no
+			printf("tina: gets \t\t%d cookies left, %d in a row\n",cookie,tina);
 		} else if (client==2){ //judy
-			if (tina==2){ //tina's had two so proceed
+			//printf("judy: req\n");
+			if (tina>=2){ //tina's had two so proceed
 				tina=0;
 				cookie--;
-				return 1;
+				result= 1;
+				printf("judy: gets \t\t%d cookies left\n",cookie);
 			} else {
-				return 2;
+				result= 2;
+				printf("judy: doesn't get \t%d cookies left\n",cookie);
 			}
 		} else {
-			perror("bad client ID") // not tina or judy...
+			perror("bad client ID"); // not tina or judy...
 		}
 	} else {
-		return 0; //no more cookies
+		printf("no more cookies!\n");
+		result= 0; //no more cookies
 	}
 
 	return &result;
@@ -40,7 +47,7 @@ void *remote_exit_1_svc(int *argp, struct svc_req *rqstp)
 {
 	//static char * result;
 
-	printf("Request for Termination Received\n");
+	printf("exitting %d\n",*argp);
     fflush(NULL);
     exit(0);
 
